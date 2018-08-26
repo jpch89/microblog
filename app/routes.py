8901c42d2c428a5d394e6b961d9e4c23074e1cec
jpch89 <jpch89@outlook.com>
2018-08-26 22:31:00 +0800

    Chapter 2

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@
 # @Author: jpch89
 # @Time:   18-8-26 下午3:33
 
+from flask import render_template
 # 第一个 app 是 app 包，是文件夹
 # 第二个 app 是 app 变量，是 Flask 核心对象
 from app import app
@@ -10,4 +11,16 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': '进击的团子'}
+    posts = [
+        {
+            'author': {'username': '路飞'},
+            'body': '今天要去救山治！'
+        },
+        {
+            'author': {'username': '乔巴'},
+            'body': '集市的棉花糖好好吃！'
+        }
+    ]
+    return render_template('index.html', title='首页', user=user, posts=posts)
+    # return render_template('index.html', user=user)
